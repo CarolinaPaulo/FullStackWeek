81df79acd82c14a25f5830c2cb63b9ac0e9ba755
CarolinaPaulo <carolinadefpaulo@gmail.com>
2021-04-05 21:24:24 -0300

    refactor: button and isVaccinated display

--- fw-web/src/components/PeopleList.js
+++ fw-web/src/components/PeopleList.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Table, Tag, Space, Button, message } from 'antd';
+import { Table, Button, message } from 'antd';
 import Column from "antd/lib/table/Column";
 import PeopleDataService from "../services/PeopleDataService";
 
@@ -28,6 +28,15 @@ export default class PeopleList extends Component{
             )
      }
 
+     success = (record) => {
+        if(record.isVaccinated){
+            record.isVaccinated = false;
+        }else record.isVaccinated = true;
+        PeopleDataService.updatePerson(record, record.PersonId);
+        message.success('Status alterado com sucesso!');
+
+     }
+
 
 
 
@@ -41,9 +50,10 @@ export default class PeopleList extends Component{
                             <Column title="CPF" dataIndex="cpf" key="cpf"/>
                             <Column title="TELEFONE" dataIndex="telefone" key="telefone"/>
                             <Column title="EMAIL" dataIndex="email" key="email"/>
-                            <Column title="IS VACCINATED" dataIndex="isVaccinated" key="isVaccinated"
-                               render={(text, record) => (<p>{String(record.isVaccinated)}</p>)}/>
-                            <Column title="UPDATE"  key="update"/>
+                            <Column title="IS VACCINATED" dataIndex="isVaccinated" key="isVaccinated" 
+                                render={(text, record) => (<p>{String(record.isVaccinated)}</p>)}/>
+                            <Column title="UPDATE"  key="update" 
+                                render={(text, record) => (<Button onClick={() => this.success(record)} type="primary">Update Status</Button>)} />
                         </Table>
                   </div>
               </div>
